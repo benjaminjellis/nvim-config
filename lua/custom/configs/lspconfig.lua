85a31830c1c628f978d99936afe7b94c73df9d72
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-02-24 12:20:11 +0000

    feat: nvchad update

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,10 +1,14 @@
 local lspconfig = require "lspconfig"
 
+-- note: to use the ocaml lsp it needs to be installed in the switch being used for a given project
 lspconfig.ocamllsp.setup {
   on_attach = function(client, _)
     client.server_capabilities.semanticTokensProvider = nil
   end,
 }
+
+-- json lsp for formatting json
 lspconfig.jsonls.setup {}
---
--- lspconfig.kotlin_language_server.setup {}
+
+-- metals is the lsp for scala, this needs to be installed to work 
+lspconfig.metals.setup {}
