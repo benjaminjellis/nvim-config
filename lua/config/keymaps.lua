6e3e306fad6577c9d1d23930f681f8c646299b33
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-03-23 11:07:25 +0000

    feat: move to lazyvim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default eymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", ";", ":", { remap = true })
+vim.keymap.set("n", "<leader>cb", "<cmd>%bd|e#<cr>")
+-- spelling
+vim.keymap.set("n", "<leader>spn", function()
+  vim.fn["spelunker#jump_next"]()
+end, { remap = true })
+vim.keymap.set("n", "<leader>spp", function()
+  vim.fn["spelunker#jump_prev"]()
+end, { remap = true })
+vim.keymap.set("n", "<leader>spl", function()
+  vim.fn["spelunker#correct_from_list"]()
+end, { remap = true })
---
+++ lua/plugins/spellcheck.lua
@@ -0,0 +1,6 @@
+return {
+  {
+    "kamykn/spelunker.vim",
+    lazy = false,
+  },
+}
