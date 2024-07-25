2ded3c544bb4f98cbc5e691eaf7db04e146a156a
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-07-26 00:15:59 +0100

    feat: add elm & update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,24 @@
 return {
   { "Saecki/crates.nvim", enabled = false },
+  { "tamasfe/taplo", enabled = false },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      "mfussenegger/nvim-dap-python",
+      -- stylua: ignore
+      keys = {
+        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+      },
+      config = function()
+        require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/bin/python"))
+        table.insert(require("dap").configurations.python, {
+          justMyCode = false,
+        })
+      end,
+    },
+  },
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
